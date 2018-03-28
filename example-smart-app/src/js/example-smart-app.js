195bab39a7174f32a72e8de89b493174f805981f
Nathan Bachus <37556874+nbachus-cred@users.noreply.github.com>
2018-03-28 13:40:16 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,13 +12,13 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
+                    type: 'DiagnosticReport',
                     query: {
                       code: {
                         /*$or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']*/
-                        $or: ['http://loinc.org|8480-6']
+                        //$or: ['http://loinc.org|8480-6']
                       }
                     }
                   });
@@ -30,23 +30,11 @@
                       }
                     }
                   });
-        var obv3 = smart.patient.api.fetchAll({
-                    type: 'DiagnosticReport',
-                    query: {
-                      code: {
-                       
-                      }
-                    }
-                  });
 
         $.when(pt, obv).fail(onError);
         
         $.when(pt, obv2).done(function(patient, obv2) {
           return JSON.parse(obv2);
-        });
-        
-        $.when(pt, obv3).done(function(patient, obv3) {
-          return JSON.parse(obv3);
         });
         
         $.when(pt, obv).done(function(patient, obv) {

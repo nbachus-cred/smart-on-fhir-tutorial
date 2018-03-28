7920a2e5ab4d4243911bc418cf930f9285d087f7
Nathan Bachus <37556874+nbachus-cred@users.noreply.github.com>
2018-03-28 14:44:36 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,7 +12,7 @@
         var patient = smart.patient;
         var pt = patient.read();
                 var obv = smart.patient.api.fetchAll({
-                    type: 'CarePlan',
+                    type: 'DiagnosticReport',
                     query: {
                       code: {
                         /*$or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',

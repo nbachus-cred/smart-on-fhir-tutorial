986f13e8a8063166848c2da8825b7a9c28a71e6d
Nathan Bachus <37556874+nbachus-cred@users.noreply.github.com>
2018-03-28 14:59:20 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,29 +12,10 @@
         var patient = smart.patient;
         var pt = patient.read();
                 var obv = smart.patient.api.fetchAll({
-                    type: 'DiagnosticReport',
+                    type: 'CarePlan',
                     query: {
-                      code: {
-                        /*$or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']*/
-                      }
-                    }
-                  },
-                                                    {
-                    type: 'Observations',
-                    query: {
-                      code: {
-                        /*$or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']*/
-                      }
-                    }
-                  },
-                                                    {
-                    type: 'DiagnosticReport',
-                    query: {
-                      code: {
+                      id: { 
+                        $or: ['7184074']
                         /*$or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']*/

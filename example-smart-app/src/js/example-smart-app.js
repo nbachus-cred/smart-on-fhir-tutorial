31a5a90e52623896fcb23dfdcacc622c9216a245
nbachus-cred <nbachus@credera.com>
2018-03-29 11:14:55 -0500

    fixes

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -66,7 +66,7 @@
 
           for (var i = 0; i < obv.length; i++) {
                 if (!obv[i].code.coding[0].display == "Entered in Error") {
-                      p.hdl = p.hdl + obv[i].code.coding[0].code + " " + obv[i].code.coding[0].display + "<br>";
+                      p.hdl = p.hdl + obv[i].dateRecorded + " " +obv[i].code.coding[0].code + " " + obv[i].code.coding[0].display + "<br>";
                 }
           }
 
